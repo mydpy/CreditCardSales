6c3acf77a0629865f295f535946a884d09842a68
Myles Baker <mylesdanielbaker@gmail.com>
2015-01-28 15:31:32 -0800

    Initial commit. Deleted my local repo so starting from scratch.

---
+++ CreditCardSales/src/v1/SalesPerStoreMapper.java
@@ -0,0 +1,20 @@
+package v1;
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class SalesPerStoreMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+  @Override
+  public void map(LongWritable key, Text value, Context context)
+      throws IOException, InterruptedException {
+
+    /*
+     * TODO implement
+     */
+
+  }
+}
---
+++ CreditCardSales/src/v1/SalesPerStoreReducer.java
@@ -0,0 +1,21 @@
+package v1;
+import java.io.IOException;
+
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class SalesPerStoreReducer extends Reducer<Text, IntWritable, Text, DoubleWritable> {
+
+  @Override
+  public void reduce(Text key, Iterable<IntWritable> values, Context context)
+      throws IOException, InterruptedException {
+
+    /*
+     * TODO implement
+     */
+
+  }
+}
